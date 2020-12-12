c5bbd5f70ca3389cb8f81669d9fe8fe621ad7d2b
GolamMorshed <45533293+GolamMorshed@users.noreply.github.com>
2020-12-12 08:46:28 +0800

    single page view

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 
+
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:http/http.dart' as http;
@@ -33,7 +36,7 @@ class _MyPageState extends State<MyHomePage> {
     var jsonData = json.decode(data.body);
     List<Item> items = [];
     for(var i in jsonData){
-      Item item = Item(i["ItemCode"],i["Description"],i["Image"]);
+      Item item = Item(i["ItemCode"],i["UserMememberNo"],i["Description"],i["Image"],i["Image1"],i["Image2"],i["Image3"],i["Image4"]);
       items.add(item);
     }
     print(items.length);
@@ -95,9 +98,15 @@ class _MyPageState extends State<MyHomePage> {
 
 class Item {
   final String ItemCode;
+  final String UserMememberNo;
   final String Description;
   final String Image;
-  Item(this.ItemCode, this.Description,this.Image);
+  final String Image1;
+  final String Image2;
+  final String Image3;
+  final String Image4;
+
+  Item(this.ItemCode, this.UserMememberNo, this.Description,this.Image,this.Image1,this.Image2,this.Image3,this.Image4);
 }
 
 class DetailPage extends StatelessWidget {
@@ -108,6 +117,166 @@ class DetailPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title:Text(item.Description),
+      ),
+
+      body: Container(
+        child:Container(
+          child: Column(
+            //gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+            children: <Widget>[
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.network("https://thegreen.studio/ecommerce/default/upload/"+item.Image,
+                      width: 450.0,
+                      height: 400.0,
+                      fit: BoxFit.cover,
+                    ),
+
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Card(
+                      child: Row(
+                        children: <Widget>[
+                          Image.network("https://thegreen.studio/ecommerce/default/upload/"+item.Image1,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                          Image.network("https://thegreen.studio/ecommerce/default/upload/"+item.Image2,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                          Image.network("https://thegreen.studio/ecommerce/default/upload/"+item.Image3,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                          Image.network("https://thegreen.studio/ecommerce/default/upload/"+item.Image4,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              Container(
+                child: SizedBox(
+                  child: Row(
+                    children: <Widget>[
+                      Text(item.UserMememberNo,style: TextStyle(
+                        fontSize:30,
+
+                      ),),
+                    ],
+                  ),
+
+                ),
+              ),
+              Container(
+                child: SizedBox(
+                  child: Row(
+                    children: <Widget>[
+                      Text("Price: RM12",style: TextStyle(
+                        fontSize:30,
+
+                      ),),
+                    ],
+                  ),
+
+                ),
+              ),
+              Container(
+                child: SizedBox(
+                  child: Row(
+                    children: <Widget>[
+                      Text(item.Description,style: TextStyle(
+                        fontSize:20,
+
+                      ),),
+                    ],
+                  ),
+
+                ),
+              ),
+
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 40,
+                        height: 32,
+                        child:OutlineButton(
+                          padding: EdgeInsets.zero,
+                          child: Icon(Icons.remove),
+                          shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(13)),
+                          //Icon(Icons.remove),
+                        )
+                    ),
+                    SizedBox(
+                        width: 40,
+                        height: 32,
+                        //padding:const EdgeInsets.symmetric(horizontal: kNoDefaultValue / 2),
+                        child:Text("01", style: Theme.of(context).textTheme.headline6,),
+                    ),
+                    SizedBox(
+                        width: 40,
+                        height: 32,
+                        child:OutlineButton(
+                          padding: EdgeInsets.zero,
+                          child: Icon(Icons.add),
+                          shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(13)),
+                          //Icon(Icons.Add),
+                        )
+                    ),
+
+                  ],
+                ),
+              ),
+
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 50,
+                      width: 58,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(18),
+                        border: Border.all(
+                        )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: 350,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+                        onPressed: (){
+
+                        },
+                        color: Colors.blue,
+                        child: Text("BUY NOW".toUpperCase(),style: TextStyle(
+                          fontSize:17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
 
 
