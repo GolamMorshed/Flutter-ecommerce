07de877f469ace4c030efd6e93a879b9406cdb9a
GolamMorshed <45533293+GolamMorshed@users.noreply.github.com>
2020-12-17 11:39:57 +0800

    view single product
    view single product based on category function don e

--- lib/main_drawer.dart
+++ lib/main_drawer.dart
@@ -143,9 +143,14 @@ class Categories{
 //CATEGORY VIEW CLASS
 class CategoryList {
   final String ItemModel;
+  final String Description;
   final String Image;
+  final String Image1;
+  final String Image2;
+  final String Image3;
+  final String Image4;
   final String UserMememberNo;
-  CategoryList(this.ItemModel,this.Image,this.UserMememberNo);
+  CategoryList(this.ItemModel,this.Image,this.UserMememberNo, this.Description, this.Image1, this.Image2, this.Image3, this.Image4);
 }
 
 class ViewCategory extends StatefulWidget{
@@ -164,7 +169,7 @@ class _ViewCategoryState extends State<ViewCategory> {
 
    List<CategoryList> cList = [];
    for(var i in jsonData) {
-     CategoryList cate = CategoryList(i["ItemModel"],i["Image"],i["UserMememberNo"]);
+     CategoryList cate = CategoryList(i["ItemModel"],i["Image"],i["UserMememberNo"],i["Image1"],i["Image2"],i["Image3"],i["Image4"],i["Description"]);
      cList.add(cate);
    }
    return cList;
@@ -195,7 +200,7 @@ class _ViewCategoryState extends State<ViewCategory> {
                   return Card(
                     child: GestureDetector(
                       onTap: (){
-
+                        Navigator.push(context, new MaterialPageRoute(builder: (context)=>DetailPage(snapshot.data[index])));
                       },
                       child: Column(
                         children: <Widget>[
@@ -220,6 +225,236 @@ class _ViewCategoryState extends State<ViewCategory> {
     );
   }
 }
+
+// //CLASS FOR SINGLE ITEM VIEW
+// class CategoryList {
+//   final String UserMememberNo;
+//   final String Description;
+//   final String Image;
+//   final String Image1;
+//   final String Image2;
+//   final String Image3;
+//   final String Image4;
+//
+//   CategoryList(this.UserMememberNo, this.Description, this.Image, this.Image1, this.Image2, this.Image3, this.Image4);
+// }
+
+class DetailPage extends StatefulWidget{
+  final CategoryList cList;
+
+  DetailPage(this.cList);
+
+
+  @override
+  _DetailPageState createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  int _count = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title:Text(widget.cList.Description),
+      ),
+
+
+
+      body: Container(
+        child:Container(
+          child: Column(
+            //gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 1),
+            children: <Widget>[
+              Card(
+                child: Column(
+                  children: <Widget>[
+                    Image.network("https://thegreen.studio/ecommerce/default/upload/"+widget.cList.Image,
+                      width: 450.0,
+                      height: 400.0,
+                      fit: BoxFit.cover,
+                    ),
+
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+                    Card(
+                      child: Row(
+                        children: <Widget>[
+                          Image.network("https://thegreen.studio/ecommerce/default/upload/"+widget.cList.Image1,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                          Image.network("https://thegreen.studio/ecommerce/default/upload/"+widget.cList.Image2,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                          Image.network("https://thegreen.studio/ecommerce/default/upload/"+widget.cList.Image3,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                          Image.network("https://thegreen.studio/ecommerce/default/upload/"+widget.cList.Image4,
+                            width: 100.0,
+                            height: 100.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+
+
+              Container(
+                child: SizedBox(
+                  child: Row(
+                    children: <Widget>[
+                      Text(widget.cList.UserMememberNo,style: TextStyle(
+                        fontSize:30,
+
+                      ),),
+                    ],
+                  ),
+
+                ),
+              ),
+              Container(
+                child: SizedBox(
+                  child: Row(
+                    children: <Widget>[
+                      Text("Price: RM12",style: TextStyle(
+                        fontSize:30,
+
+                      ),),
+                    ],
+                  ),
+
+                ),
+              ),
+              Container(
+                child: SizedBox(
+                  child: Row(
+                    children: <Widget>[
+                      Text(widget.cList.Description,style: TextStyle(
+                        fontSize:20,
+
+                      ),),
+                    ],
+                  ),
+
+                ),
+              ),
+
+              Container(
+
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(
+                        width: 40,
+                        height: 32,
+                        child:OutlineButton(
+                          onPressed: (){
+                            setState(() {
+                              if(_count <= 1)
+                              {
+                                _count = 1;
+                              }
+
+                              _count--;
+                            });
+
+                          },
+                          padding: EdgeInsets.zero,
+                          child: Icon(Icons.remove),
+                          shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(13)),
+                          //Icon(Icons.remove),
+                        )
+                    ),
+                    SizedBox(
+                      width: 40,
+                      height: 32,
+                      //padding:const EdgeInsets.symmetric(horizontal: kNoDefaultValue / 2),
+                      child:Text("$_count", style: Theme.of(context).textTheme.headline6),
+                    ),
+                    SizedBox(
+                        width: 40,
+                        height: 32,
+                        child:OutlineButton(
+                          onPressed: (){
+                            setState(() {
+                              _count++;
+                            });
+
+                          },
+                          padding: EdgeInsets.zero,
+                          child: Icon(Icons.add),
+                          shape:RoundedRectangleBorder(borderRadius: BorderRadius.circular(13)),
+                          //Icon(Icons.Add),
+                        )
+                    ),
+
+                  ],
+                ),
+              ),
+
+              Container(
+                child: Row(
+                  children: <Widget>[
+                    Container(
+                      height: 50,
+                      width: 58,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(18),
+                          border: Border.all(
+                          )
+                      ),
+                    ),
+                    SizedBox(
+                      height: 50,
+                      width: 350,
+                      child: FlatButton(
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+                        onPressed: (){
+
+                        },
+                        color: Colors.blue,
+                        child: Text("BUY NOW".toUpperCase(),style: TextStyle(
+                          fontSize:17,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white,
+                        ),),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                child: Column(
+                  children: <Widget>[
+
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+
+
+    );
+  }
+}
+
+
+
+
 
 
 
